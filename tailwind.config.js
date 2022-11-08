79235ae4fce7e854acedc1639f5efccd444200bd
Mridul Vasudeva <88312870+Mridul1703@users.noreply.github.com>
2022-11-08 23:54:13 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontfamily: {
+        'Roboto': ['Roboto', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
